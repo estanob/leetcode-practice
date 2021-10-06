6e2c469824d9e072bab56b49b1c1d6d0887bd1d2
Brandon <estanob@gmail.com>
2021-10-06 15:57:37 -0700

    More array practice

--- easy/array_practice.js
+++ easy/array_practice.js
@@ -1,8 +1,8 @@
 /** Contains Duplicate */
 
-let nums1 = [1,2,3,1];
-let nums2 = [1,2,3,4];
-let nums3 = [1,1,1,3,3,4,3,2,4,2];
+// let nums1 = [1,2,3,1];
+// let nums2 = [1,2,3,4];
+// let nums3 = [1,1,1,3,3,4,3,2,4,2];
 
 var containsDuplicate = function(nums) {
   for (let i = 0; i < nums.length - 1; i++) {
@@ -15,9 +15,9 @@ var containsDuplicate = function(nums) {
   return false
 };
 
-console.log("Test Case1: ", containsDuplicate(nums1));
-console.log("Test Case2: ", containsDuplicate(nums2));
-console.log("Test Case3: ", containsDuplicate(nums3));
+// console.log("Test Case1: ", containsDuplicate(nums1));
+// console.log("Test Case2: ", containsDuplicate(nums2));
+// console.log("Test Case3: ", containsDuplicate(nums3));
 
 
 
@@ -142,3 +142,137 @@ var twoSum = function(nums, target) {
 // console.log("Test case1: ", twoSum(nums1, target1));
 // console.log("Test case2: ", twoSum(nums2, target2));
 // console.log("Test case3: ", twoSum(nums3, target3));
+
+
+
+
+
+
+
+
+
+/**Two Sum II - Input array is sorted */
+// let numbers1 = [2,7,11,15];
+// let target1 = 9;
+
+// let numbers2 = [2,3,4];
+// let target2 = 6;
+
+// let numbers3 = [-1,0];
+// let target3 = -1;
+
+var twoSum = function(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (i < j && numbers[i] + numbers[j] === target) {
+        return [(i + 1), (j + 1)];
+      }
+    }
+  }
+}
+
+// console.log("Test Case1: ", twoSum(numbers1, target1));
+// console.log("Test Case2: ", twoSum(numbers2, target2));
+// console.log("Test Case3: ", twoSum(numbers3, target3));
+
+
+
+
+
+
+
+// let nums1 = [1,2,3,1];
+// let k1 = 3;
+
+// let nums2 = [1,0,1,1];
+// let k2 = 1;
+
+// let nums3 = [1,2,3,1,2,3];
+// let k3 = 2;
+
+var containsNearbyDuplicate = function (nums, k) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i < j && nums[i] === nums[j] && Math.abs(i - j) <= k) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// console.log("Test Case1: ", containsNearbyDuplicate(nums1, k1))
+// console.log("Test Case2: ", containsNearbyDuplicate(nums2, k2))
+// console.log("Test Case3: ", containsNearbyDuplicate(nums3, k3))
+
+
+
+
+
+
+
+/** Missing Number */
+
+// let nums1 = [3,0,1];
+// let nums2 = [0,1];
+// let nums3 = [9,6,4,2,3,5,7,0,1];
+// let nums4 = [0];
+
+var missingNumber = function (nums) {
+  for (let i = 0; i <= nums.length; i++) {
+    if (!nums.includes(i)) return i;
+  }
+}
+
+// console.log("Test Case1: ", missingNumber(nums1))
+// console.log("Test Case2: ", missingNumber(nums2))
+// console.log("Test Case3: ", missingNumber(nums3))
+// console.log("Test Case4: ", missingNumber(nums4))
+
+
+
+
+
+
+/** Move Zeroes */
+// let nums1 = [0,1,0,3,12];
+// let nums2 = [0];
+
+var moveZeroes = function(nums) {
+  let organized = [];
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]
+    if (num !== 0) organized.push(num)
+  }
+  for (let j = 0; j <= (nums.length - organized.length); j++) {
+    organized.push(0)
+  }
+  return organized;
+}
+
+// console.log("Test Case1: ", moveZeroes(nums1));
+// console.log("Test Case2: ", moveZeroes(nums2));
+
+
+
+
+
+
+
+/** Find All Numbers Disappeared in an Array */
+
+// let nums1 = [4,3,2,7,8,2,3,1];
+// let nums2 = [1,1];
+
+var findDisappearedNumbers = function(nums) {
+  let missingNums = [];
+  for (let i = 1; i <= nums.length; i++) {
+    if (!nums.includes(i) && !missingNums.includes(i)) {
+      missingNums.push(i)
+    }
+  }
+  return missingNums;
+}
+
+// console.log("Test Case1: ", findDisappearedNumbers(nums1))
+// console.log("Test Case2: ", findDisappearedNumbers(nums2))
