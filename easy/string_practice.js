67ad0f5b452ca790093ed02eaed488a1a338fbad
Brandon <estanob@gmail.com>
2021-10-07 16:12:53 -0700

    String practice

--- easy/string_practice.js
+++ easy/string_practice.js
@@ -194,6 +194,246 @@ var fizzBuzz = function(n) {
 
 
 
+/** Jewels and Stones */
+// let jewels1 = "aA";
+// let stones1 = "aAAbbbb";
+
+// let jewels2 = "z";
+// let stones2 = "ZZ";
+
+var numJewelsInStones = function (jewels, stones) {
+  let numJewels = 0;
+  let jewelTypes = jewels.split("");
+  for (let i = 0; i < stones.length; i++) {
+    if (jewelTypes.includes(stones[i])) numJewels++
+  }
+  return numJewels;
+}
+
+// console.log("Test Case1:", numJewelsInStones(jewels1, stones1));
+// console.log("Test Case:2", numJewelsInStones(jewels2, stones2));
+
+
+
+
+
+
+
+
+
+
+
+
+/** Shuffle String */
+
+// let s1 = "codeleet";
+// let indices1 = [4,5,6,7,0,2,1,3];
+
+// let s2 = "abc";
+// let indices2 = [0,1,2];
+
+// let s3 = "aiohn";
+// let indices3 = [3,1,4,2,0];
+
+// let s4 = "aaiougrt";
+// let indices4 = [4,0,2,6,7,3,1,5];
+
+// let s5 = "art";
+// let indices5 = [1,0,2];
+
+var restoreString = function (s, indices) {
+  let restoredStr = [];
+  for (let i = 0; i < indices.length; i++) {
+    restoredStr[indices[i]] = s[i];
+  }
+  return restoredStr.join("");
+}
+
+// console.log("Test Case1:", restoreString(s1, indices1));
+// console.log("Test Case2:", restoreString(s2, indices2));
+// console.log("Test Case3:", restoreString(s3, indices3));
+// console.log("Test Case4:", restoreString(s4, indices4));
+// console.log("Test Case:5", restoreString(s5, indices5));
+
+
+
+
+
+
+
+
+
+
+
+/** Count the Number of Consistent Strings */
+
+// let allowed1 = "ab";
+// let words1 = ["ad","bd","aaab","baa","badab"];
+
+// let allowed2 = "abc";
+// let words2 = ["a","b","c","ab","ac","bc","abc"];
+
+// let allowed3 = "cad";
+// let words3 = ["cc","acd","b","ba","bac","bad","ac","d"];
+
+var countConsistentStrings = function (allowed, words) {
+  let numStr = 0;
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i]
+    if (strIsAllowed(allowed, word)) numStr++;
+  }
+  return numStr;
+}
+
+var strIsAllowed = function (allowed, word) {
+  const allowedChars = allowed.split("")
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+    if (!allowedChars.includes(char)) return false;
+  }
+  return true;
+}
+
+// console.log("Test Case1:", countConsistentStrings(allowed1, words1));
+// console.log("Test Case2:", countConsistentStrings(allowed2, words2));
+// console.log("Test Case3:", countConsistentStrings(allowed3, words3));
+
+
+
+
+
+
+const upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+// let s1 = "Hello";
+// let s2 = "here";
+// let s3 = "LOVELY";
+
+var toLowerCase = function (s) {
+  let resultStr = "";
+  for (let v = 0; v < s.length; v++) {
+    let char = s[v];
+    if (upperCaseAlphabet.includes(char)) {
+      resultStr += alphabet[upperCaseAlphabet.indexOf(char)];
+    } else {
+      resultStr += char;
+    }
+  }
+  return resultStr;
+}
+
+// console.log("Test Case1:", toLowerCase (s1));
+// console.log("Test Case2:", toLowerCase (s2));
+// console.log("Test Case3:", toLowerCase (s3));
+
+
+
+
+
+/** Unique Morse Code Words */
+const morseLetters = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+// let words1 = ["gin","zen","gig","msg"];
+// let words2 = ["a"];
+
+var uniqueMorseRepresentations = function (words) {
+  if (words.length === 1) return 1
+  let reps = [];
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (!reps.includes(convertToMorse(word))) reps.push(convertToMorse(word))
+  }
+  return reps.length;
+}
+
+var convertToMorse = function (word) {
+  let morseWord = "";
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+    morseWord += morseLetters[alphabet.indexOf(char)]
+  }
+  return morseWord;
+}
+
+// console.log("Test Case1:", uniqueMorseRepresentations(words1));
+// console.log("Test Case2:", uniqueMorseRepresentations(words2));
+
+
+
+
+
+
+
+/** Reverse Prefix of Word */
+// let word1 = "abcdefd";
+// let ch1 = "d";
+
+// let word2 = "xyxzxe";
+// let ch2 = "z";
+
+// let word3 = "abcd";
+// let ch3 = "z";
+
+var reversePrefix = function (word, ch) {
+  let chars = word.split("");
+  if (!chars.includes(ch)) return word;
+  let revPref = "";
+  let keyIdx = chars.indexOf(ch);
+  revPref += reversePhrase(word.slice(0, keyIdx + 1));
+  revPref += word.slice(keyIdx + 1);
+  return revPref;
+}
+
+var reversePhrase = function (phrase) {
+  let revWord = "";
+  for (let i = phrase.length - 1; i >= 0; i--) {
+    revWord += phrase[i]
+  }
+  return revWord;
+}
+
+// console.log("Test Case1:", reversePrefix (word1, ch1));
+// console.log("Test Case2:", reversePrefix (word2, ch2));
+// console.log("Test Case:3", reversePrefix (word3, ch3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -243,6 +483,31 @@ var fizzBuzz = function(n) {
 /*************
  * NEED TO REVIEW
  * ******************/
+/** Check if the Sentence is Paragram */
+// let sentence1 = "thequickbrownfoxjumpsoverthelazydog";
+// let sentence2 = "leetcode";
+
+var checkIfPangram = function (sentence) {
+  // if (sentence.length < 26) return false;
+  // let sentChars = sentence.split("")
+  // for (let i = 0; i < alphabet.length; i++) {
+  //   if (!alphabet.includes([i])) return false
+  // }
+  // return true;
+
+  const set = new Set(sentence)
+  return set.size === 26;
+}
+
+// console.log("Test Case1:", checkIfPangram (sentence1));
+// console.log("Test Case2:", checkIfPangram (sentence2));
+// console.log("Test Case3:", checkIfPangram ("jwtucoucmdfwxxqnxzkaxoglszmfrcvjoiunqqausaxxaaijyqdqgvdnqcaihwilqkpivenpnekioyqujrdrovqrlxovcucjqzjsxmllfgndfprctxvxwlzjtciqxgsxfwhmuzqvlksyuztoetyjugmswfjtawwaqmwyxmvo"));
+
+
+
+
+
+
 
 // let s1 = "is2 sentence4 This1 a3";
 // let s2 = "Myself2 Me1 I4 and3";
