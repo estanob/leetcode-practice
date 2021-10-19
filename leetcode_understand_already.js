433c9a0e0e4c16e0f1af020667fbb46c92bfbe6d
Brandon <estanob@gmail.com>
2021-10-18 19:59:51 -0700

    MMore practice

--- leetcode_understand_already.js
+++ leetcode_understand_already.js
@@ -33,9 +33,9 @@ var containsDuplicate = function (nums) {
 
 
 /** 3 Sum */
-let nums1 = [-1,0,1,2,-1,-4];
-let nums2 = [];
-let nums3 = [0];
+// let nums1 = [-1,0,1,2,-1,-4];
+// let nums2 = [];
+// let nums3 = [0];
 
 var threeSum = function (nums) {
   if (nums.length < 3) return [];
@@ -43,13 +43,9 @@ var threeSum = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length - 2; j++) {
       for (let k = 0; k < nums.length - 1; k++) {
-        if (/**i !== j && j !== k && i !== k &&*/ i < j && j < k && i < k) {
-        // if (i < j && j < k && i !== j && j !== k && i !== k) {
-          // if (nums[i] + nums[j] + nums[k] === 0) threeResult.push([nums[i], nums[j], nums[k]])
+        if (i < j && j < k && i < k) {
           if (nums[i] + nums[j] + nums[k] === 0) {
             let sumAns = [nums[i], nums[j], nums[k]].sort((a, b) => a - b)
-            // let sumAns = [nums[i], nums[j], nums[k]]
-            // sumAns = sumAns.sort((a, b) => a - b)
             if (!threeResult.includes(sumAns)) threeResult.push(sumAns)
           }
         }
@@ -59,6 +55,125 @@ var threeSum = function (nums) {
   return threeResult;
 }
 
-console.log("Test Case1:", threeSum(nums1));
-console.log("Test Case2:", threeSum(nums2));
-console.log("Test Case3:", threeSum(nums3));
+// console.log("Test Case1:", threeSum(nums1));
+// console.log("Test Case2:", threeSum(nums2));
+// console.log("Test Case3:", threeSum(nums3));
+
+
+
+
+
+/** Missing Nummer */
+// let nums1 = [3,0,1];
+// let nums2 = [0,1];
+// let nums3 = [9,6,4,2,3,5,7,0,1];
+// let nums4 = [0];
+
+var missingNumber = function(nums) {
+  for (let i = 0; i <= nums.length; i++) {
+    if (!nums.includes(i)) return i;
+  }
+};
+
+
+
+
+
+
+
+/** Valid Palindrome */
+// let s = "A man, a plan, a canal: Panama";
+// let s = "race a car";
+
+var isPalindrome = function(s) {
+  let lowerCaseLetters = s.toLowerCase().split("");
+    let allLetters = [];
+    for (let i = 0; i < lowerCaseLetters.length; i++) {
+    if (alphabet.includes(lowerCaseLetters[i])) {
+      allLetters.push(lowerCaseLetters[i])
+    }
+  }
+  return allLetters.join("") === allLetters.reverse().join("")
+};
+
+// const alphabet = [
+//   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+//   'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+// ];
+
+
+
+
+
+
+
+
+
+/** House Robber */
+// let nums1 = [1,2,3,1];
+// let nums2 = [2,7,9,3,1];
+
+var rob = function (nums) {
+  let maxSteal = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i < j && j - i > 1) {
+        maxSteal = Math.max(maxSteal, nums[i] + nums[j])
+      }
+    }
+  }
+  return maxSteal;
+}
+
+// console.log("Test Case1:", rob(nums1));
+// console.log("Test Case2:", rob(nums2));
+
+
+
+
+
+
+
+
+
+
+
+/** Word Break */
+let s1 = "leetcode"
+let wordDict1 = ["leet","code"];                     // true
+
+let s2 = "applepenapple"
+let wordDict2 = ["apple","pen"];               //  true  
+
+let s3 = "catsandog"
+let wordDict3 = ["cats","dog","sand","and","cat"]  // false
+
+
+var wordBreak = function (s, wordDict) {
+  let sCopy = s;
+  let copyHolder;
+  const howManyWords = wordDict.length;
+  // for (let i = 0; i < s.length; i++) {
+  //   if (sCopy.includes(wordDict[i % wordDict.length])) {
+  //     sCopy = sCopy.slice(0, sCopy.indexOf(wordDict[i % wordDict.length]), wordDict[i % wordDict.length].length)
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  while (sCopy.length > 0) {
+    for (let i = 0; i < s.length; i++) {
+      if (sCopy.includes(wordDict[i % howManyWords])) {
+        copyHolder = sCopy;
+        sCopy = copyHolder.slice(0, sCopy.indexOf(wordDict[i % howManyWords]))
+        sCopy += copyHolder.slice(sCopy.indexOf(wordDict[i % howManyWords]) + 1)
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+console.log("Test Case1:", wordBreak(s1, wordDict1));
+console.log("Test Case2:", wordBreak(s2, wordDict2));
+console.log("Test Case3:", wordBreak(s3, wordDict3));
--- need_practice.js
+++ need_practice.js
@@ -20,7 +20,46 @@
  
  // console.log("Test Case1:", maxProfit(prices1));
  // console.log("Test Case2:", maxProfit(prices2));
- */
+
+
+
+
+
+
+Valid Anagram
+let s1 = "anagram", t1 = "nagaram";
+let s2 = "rat", t2 = "car";
+
+var isAnagram = function (s, t) {
+  let sLetters = {};
+  if (s.length !== t.length) return false;
+  for (let i = 0; i < s.length; i++) {
+    sLetters[s[i]] = sLetters[s[i]] ? sLetters[s[i]] + 1 : 1;
+    sLetters[t[i]] = sLetters[t[i]] ? sLetters[t[i]] - 1 : -1;
+  }
+  for (let letters in sLetters) {
+    if (sLetters[letters] !== 0) return false;
+  };
+  return true;
+}
+
+console.log("Test Case1:", isAnagram(s1, t1));
+console.log("Test Case2:", isAnagram(s2, t2));
+ 
+ 
+
+
+
+
+
+Search in Rotated Sorted Array
+let nums1 = [4,5,6,7,0,1,2], target1 = 0
+let nums2 = [4,5,6,7,0,1,2], target 2= 3
+
+var search = function(nums, target) {
+  return nums.indexOf(target)
+};
+
 
 
 
@@ -42,23 +81,72 @@
 
 var productExceptSelf = function (nums) {
   let products = [];
-  let numsCopy = nums
-  for (let i = 0; i < numsCopy.length; i++) {
-    let selfIdx = i;
-    products.push(multiplyOthers(numsCopy, selfIdx))
+  for (let i = 0; i < nums.length; i++) {
+    products.push(parseInt(multiplyOthers(nums, i)))
   }
   return products;
 }
 
 var multiplyOthers = function (arr, selfIdx) {
   let multAns = 1;
-  let reducedArr = arr.splice(selfIdx);
-  for (let i = 0; i < reducedArr.length; i++) {
-    let indivNum = reducedArr[i]
-    multAns *= indivNum;
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== selfIdx) {
+      multAns *= arr[i]
+    }
   }
   return multAns;
 }
 
 // console.log("Test Case1:", productExceptSelf(nums1));
 // console.log("Test Case2:", productExceptSelf(nums2));
+
+
+
+
+
+
+
+
+/** Container with Most Water */
+// let height1 = [1,8,6,2,5,4,8,3,7];
+// let height2 = [1,1];
+// let height3 = [4,3,2,1,4];
+// let height4 = [1,2,1];
+
+var maxArea = function (height) {
+  let max = 0;
+  for (let i = 0; i < height.length; i++) {
+    for (let j = height.length - 1; j >= 0; j--) {
+      if (i < j && ((j - i) * Math.min(height[i], height[j]) > max)) {
+        max = (j - i) * Math.min(height[i], height[j])
+      }
+    }
+  }
+  return max;
+}
+
+// console.log("Test Case1:", maxArea(height1));
+// console.log("Test Case2:", maxArea(height2));
+// console.log("Test Case3:", maxArea(height3));
+// console.log("Test Case4:", maxArea(height4));
+
+
+
+
+
+
+
+
+
+/** Find Minimum in Rotated Sorted Array */
+// let nums1 = [3,4,5,1,2];
+// let nums2 = [4,5,6,7,0,1,2];
+// let nums3 = [11,13,15,17];
+
+var findMin = function (nums) {
+  return nums.sort((a, b) => a - b)[0];
+}
+
+// console.log("Test Case1:", findMin(nums1));
+// console.log("Test Case2:", findMin(nums2));
+// console.log("Test Case3:", findMin(nums3));
