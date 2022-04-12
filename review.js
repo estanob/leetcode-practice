c23828299c29d87ff2749db90aa8eb01093f6da1
Brandon <estanob@gmail.com>
2022-04-12 13:39:22 -0700

    More pyramid practicce, using iteration and n size

--- review.js
+++ review.js
@@ -1,5 +1,5 @@
 /** Pyramid Problem */
-var createPyramid = function (nums) {
+var createNumberPyramid = function (nums) {
   let pyramidArr = [nums];
   while (pyramidArr.length < nums.length) {
     let currentLayer = pyramidArr[0];
@@ -18,10 +18,47 @@ var createLayer = function (nums) {
   return layer;
 }
 
-let nums1 = [4, 6, 5, 10, 99];
-let nums2 = [3, 3, 3];
-let nums3 = [4, 4, 8, 8, 6, 6, 6];
+// let nums1 = [4, 6, 5, 10, 99];
+// let nums2 = [3, 3, 3];
+// let nums3 = [4, 4, 8, 8, 6, 6, 6];
 
-console.log("Test Case1:", createPyramid(nums1));
-console.log("Test Case2:", createPyramid(nums2));
-console.log("Test Case3:", createPyramid(nums3));
+// console.log("Test Case1:", createNumberPyramid(nums1));
+// console.log("Test Case2:", createNumberPyramid(nums2));
+// console.log("Test Case3:", createNumberPyramid(nums3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** Create N Size Pyramid */
+var createNSizePyramid = function (n) {
+  let nPyramid = [];
+  let i = 1;
+  while (i <= n) { // setting a counter
+    let pyramidLayer = []; // layer to be pushed to final pyramid
+    for (let j = 0; j < i; j++) pyramidLayer.push("#"); // # is element of each pyramid layer, and pushed until j === i
+    nPyramid.push(pyramidLayer); // push created layer to final pyramid
+    pyramidLayer = []; // empty layer for new iteration
+    i++; // iterating, so increase i by 1 until i === n
+  }
+  return nPyramid;
+};
+
+let n1 = 10;
+let n2 = 3;
+let n3 = 8;
+
+console.log("Test Case1:", createNSizePyramid(n1));
+console.log("Test Case2:", createNSizePyramid(n2));
+console.log("Test Case3:", createNSizePyramid(n3));
