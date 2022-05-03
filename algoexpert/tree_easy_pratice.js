7d9aa09d12490c69a4e8a760642e798b4e486816
Brandon <estanob@gmail.com>
2022-05-02 17:52:15 -0700

    Stack and tree practice

---
+++ algoexpert/tree_easy_pratice.js
@@ -0,0 +1,9 @@
+class BST {
+  constructor (value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  };
+};
+
+/** Find Closest Value in BST */
--- easy/stack_practice.js
+++ easy/stack_practice.js
@@ -1,3 +1,70 @@
+/** Remove All Adjacent Duplicates in a String */
+var removeDuplicates = function (s) {
+  let noDupArr = [];
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char === noDupArr[noDupArr.length - 1]) {
+      noDupArr.pop();     // remove chars as we go
+    } else {
+      noDupArr.push(char);  // if the char is not equal to noDupArr's last char, we will push it into the array
+    };
+  };
+  return noDupArr.join("");
+  
+  
+  
+  
+  
+  
+  // let noDupStr = s;
+  // let allDupsOut = false;
+  // let idx = 0;
+  // while (!allDupsOut) {
+  //   // console.log("No Dup Str", noDupStr);
+  //   if (noDupStr.length === 2 && noDupStr[0] === noDupStr[1]) return '';
+  //   if (noDupStr[idx] === noDupStr[idx + 1]) {
+  //     noDupStr = noDupStr.slice(0, idx) + noDupStr.slice(idx + 2)
+  //     idx = 0;
+  //   } else {
+  //     idx++
+  //   };
+  //   if (idx === noDupStr.length - 1) allDupsOut = true;
+  // };
+  // return noDupStr
+}
+
+let s1 = "abbaca";
+let s2 = "azxxzy";
+
+console.log("Test Case1:", removeDuplicates(s1));
+console.log("Test Case2:", removeDuplicates(s2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /** Baseball Game */
 var calPoints = function(ops) {
   let scoreCard = [];
@@ -15,8 +82,8 @@ var calPoints = function(ops) {
   return scoreCard.reduce((a,v) => a + v, 0)
 };
 
-let ops1 = ["5","2","C","D","+"];
-let ops2 = ["5","-2","4","C","D","9","+","+"];
+// let ops1 = ["5","2","C","D","+"];
+// let ops2 = ["5","-2","4","C","D","9","+","+"];
 
-console.log("Test Case1:", calPoints(ops1));
-console.log("Test Case2:", calPoints(ops2));
+// console.log("Test Case1:", calPoints(ops1));
+// console.log("Test Case2:", calPoints(ops2));
