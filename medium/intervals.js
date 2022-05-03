a2f08dfd367230c77ba5766e03c72016650c27de
Brandon <estanob@gmail.com>
2022-05-03 16:40:17 -0700

    Insert interval algorithm

--- medium/intervals.js
+++ medium/intervals.js
@@ -1,5 +1,5 @@
 /** Merge Intervals */
-var merge = function (intervals) {
+var mergeIntervals = function (intervals) {
   intervals.sort((a, b) => a[0] - b[0]); // sort intervals by start in ascending order
   let mergedIntervals = [intervals[0]]
   // console.log(intervals);
@@ -22,10 +22,40 @@ var merge = function (intervals) {
   return mergedIntervals
 }
 
-let intervals1 = [[1,3],[2,6],[8,10],[15,18]];
-let intervals2 = [[1,4],[4,5]];
+// let intervals1 = [[1,3],[2,6],[8,10],[15,18]];
+// let intervals2 = [[1,4],[4,5]];
 
-console.log("Test Case1:", merge(intervals1));
-console.log("Test Case2:", merge(intervals2));
-console.log("Test Case3:", merge([[1,4],[2,3]]));
-console.log("Test Case4:", merge([[2,3],[2,2],[3,3],[1,3],[5,7],[2,2],[4,6]]));
+// console.log("Test Case1:", mergeIntervals(intervals1));
+// console.log("Test Case2:", mergeIntervals(intervals2));
+// console.log("Test Case3:", mergeIntervals([[1,4],[2,3]]));
+// console.log("Test Case4:", mergeIntervals([[2,3],[2,2],[3,3],[1,3],[5,7],[2,2],[4,6]]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** Insert */
+var insert = function (intervals, newInterval) {
+  let rawInterval = [...intervals, newInterval]; // ... to deconstruct intervals array
+  return mergeIntervals(rawInterval);
+}
+
+let intervals1 = [[1,3],[6,9]], newInterval1 = [2,5];
+let intervals2 = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval2 = [4,8];
+
+console.log("Test Case1", insert(intervals1, newInterval1));
+console.log("Test Case2", insert(intervals2, newInterval2));
