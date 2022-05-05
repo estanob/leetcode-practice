d4137b74f7ad91181ceaecc40756e8580ba2c15e
Brandon <estanob@gmail.com>
2022-05-04 20:55:32 -0700

    Debugged and solved Last Stone Weight

--- easy/heap.js
+++ easy/heap.js
@@ -26,25 +26,96 @@ var getStrength = function (row) {
   return strength
 }
 
-let mat1 = 
-  [
-    [1,1,0,0,0],
-    [1,1,1,1,0],
-    [1,0,0,0,0],
-    [1,1,0,0,0],
-    [1,1,1,1,1]
-  ], 
-  k1 = 3;
+// let mat1 = 
+//   [
+//     [1,1,0,0,0],
+//     [1,1,1,1,0],
+//     [1,0,0,0,0],
+//     [1,1,0,0,0],
+//     [1,1,1,1,1]
+//   ], 
+//   k1 = 3;
 
 
-let mat2 = 
-  [
-    [1,0,0,0],
-    [1,1,1,1],
-    [1,0,0,0],
-    [1,0,0,0]
-  ], 
-  k2 = 2;
+// let mat2 = 
+//   [
+//     [1,0,0,0],
+//     [1,1,1,1],
+//     [1,0,0,0],
+//     [1,0,0,0]
+//   ], 
+//   k2 = 2;
 
-  console.log("Test Case1: ", kWeakestRows(mat1, k1));
-  console.log("Test Case2: ", kWeakestRows(mat2, k2));
+//   console.log("Test Case1: ", kWeakestRows(mat1, k1));
+//   console.log("Test Case2: ", kWeakestRows(mat2, k2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** Last Stone Weight */
+function lastStoneWeight(stones) {
+  while (stones.length > 1) {
+  // while (stones.length !== 0 && stones.length !== 1) {
+    console.log("Before sort", stones);
+    stones = stones.sort((a, b) => a - b);
+    console.log("After sort", stones);
+    let lastStone = stones[stones.length - 1];
+    let secondToLastStone = stones[stones.length - 2];
+    console.log("Did the sort stick?", stones);
+    if (lastStone === secondToLastStone) {
+      stones.pop()
+      stones.pop()
+      console.log("after popping", stones);
+    } else {
+      stones.unshift(Math.abs(lastStone - secondToLastStone))
+      stones.pop()
+      stones.pop()
+      console.log("What is stones here?", stones);
+      stones = stones.sort((a, b) => a - b);
+      console.log("after popping", stones);
+    }
+  }
+  console.log("Out of while", stones);
+  if (stones.length === 1) {
+    return stones.pop()
+  } else {
+    return 0
+  }
+}
+
+let stones1 = [2,7,4,1,8,1]
+let stones2 = [1]
+
+console.log("Last Stone Weight Test Case1: ", lastStoneWeight(stones1));
+console.log("Last Stone Weight Test Case2: ", lastStoneWeight(stones2));
+console.log("Last Stone Weight Test Case3: ", lastStoneWeight([3,7,2]));
