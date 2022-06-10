d288057e241c6d0b4c8be2fa9b2ba7e92609e3c4
Brandon <estanob@gmail.com>
2022-06-10 16:55:43 -0700

    Structy tree practice

--- structy/binary_tree.js
+++ structy/binary_tree.js
@@ -7,22 +7,12 @@ class Node {
 }
 
 
-/** All Tree Paths */
-const allTreePaths = root => {
-  if (!root) return [] // empty arr if root is undefined
-  if (!root.left && !root.right) return [[root.val]] // if there is only a node but no left and right, return array containing only array with root.val
-  
-  let paths = []
 
-  const leftPath = allTreePaths(root.left)
-  for (let sub of leftPath) paths.push([root.val, ...sub]) // splat to decompose the sub arr and push in as part of the array already containg root.val
 
-  const rightPath = allTreePaths(root.right)
-  for (let sub of rightPath) paths.push([root.val, ...sub])
-  // console.log("Paths:", paths);
 
-  return paths;
-}
+
+
+
 
 const a = new Node('a');
 const b = new Node('b');
@@ -43,12 +33,124 @@ c.right = f;
 //  / \     \
 // d   e     f
 
+
+
+/** Depth First Values */
+const depthFirstValues = (root) => {
+  let values = []
+  let stack = [root]
+  while (stack.length > 0) {
+    const node = stack.pop()
+    values.push(node.val)
+
+    if (node.right) stack.push(node.right)
+    if (node.left) stack.push(node.left)
+  }
+  return values
+}
+
+// console.log("Test Case1:", depthFirstValues(a));
+
+
+
+
+
+/** Breadth First Values */
+const breadthFirstValues = (root) => {
+  if (!root) return []
+  let values = []
+  let queue = [root]
+  while (queue.length > 0) {
+    const node = queue.shift()
+    values.push(node.val)
+
+    if (node.left) queue.push(node.left)
+    if (node.right) queue.push(node.right)
+  }
+  return values
+}
+
+console.log(breadthFirstValues(a));
+
+
+
+
+
+/** All Tree Paths */
+/*
+            a
+           / \
+          b  c
+        / \   \
+       d  e    f
+*/
+const allTreePaths = root => {
+  if (!root) return [] // empty arr if root is undefined
+  if (!root.left && !root.right) return [[root.val]] // if there is only a node but no left and right, return array containing only array with root.val
+  
+  let paths = []
+
+  const leftPath = allTreePaths(root.left)
+  for (let sub of leftPath) paths.push([root.val, ...sub]) // splat to decompose the sub arr and push in as part of the array already containg root.val
+
+  const rightPath = allTreePaths(root.right)
+  for (let sub of rightPath) paths.push([root.val, ...sub])
+  console.log("Paths:", paths);
+
+  return paths;
+}
+
 // allTreePaths(a); // ->
 // [ 
-//   [ 'a', 'b', 'd' ], 
-//   [ 'a', 'b', 'e' ], 
-//   [ 'a', 'c', 'f' ] 
-// ] 
+  //   [ 'a', 'b', 'd' ], 
+  //   [ 'a', 'b', 'e' ], 
+  //   [ 'a', 'c', 'f' ] 
+  // ] 
+  
+  // console.log("Node A:", a);
+  console.log("Test Case1:", allTreePaths(a));
 
-// console.log("Node A:", a);
-console.log("Test Case1:", allTreePaths(a));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /** Tree Sum */
+const treeSum = (root) => {
+  if (!root) return 0
+  
+  let sum = 0
+  const queue = [root]
+  
+  while (queue.length > 0) {
+    const currentNode = queue.shift()
+    sum += currentNode.val
+    if (currentNode.left) queue.push(currentNode.left)
+    if (currentNode.right) queue.push(currentNode.right)
+  }
+  
+  return sum;
+};
