6473e4aa0e2d2c2f3c9a4d78d00b085529a69030
Brandon <estanob@gmail.com>
2022-06-17 17:44:53 -0700

    Structy binary  tree practice

--- structy/binary_tree.js
+++ structy/binary_tree.js
@@ -84,16 +84,28 @@ console.log(breadthFirstValues(a));
         / \   \
        d  e    f
 */
+
+// DFS recursive
+/*
+Approach:
+Base Case: 
+    If we have a null node, we return []
+Other base case:
+    for a node D that has no left or right child, return [['D']] 
+
+Recursive call for every node of the tree
+*/
+
 const allTreePaths = root => {
   if (!root) return [] // empty arr if root is undefined
-  if (!root.left && !root.right) return [[root.val]] // if there is only a node but no left and right, return array containing only array with root.val
+  if (!root.left && !root.right) return [[root.val]] // if there is only a node but no left and right, return 2D array containing only array with root.val
   
   let paths = []
 
-  const leftPath = allTreePaths(root.left)
+  const leftPath = allTreePaths(root.left)                // we expect 2D array
   for (let sub of leftPath) paths.push([root.val, ...sub]) // splat to decompose the sub arr and push in as part of the array already containg root.val
 
-  const rightPath = allTreePaths(root.right)
+  const rightPath = allTreePaths(root.right)              // we expect 2D array
   for (let sub of rightPath) paths.push([root.val, ...sub])
   console.log("Paths:", paths);
 
@@ -196,4 +208,172 @@ const treeValueCount = (root, target) => {
   }
   
   return count
+};
+
+
+
+
+
+/* Tree Min Value */
+const treeMinValue = (root) => {
+  // BFS approach
+  let min = Infinity
+  let queue = [root]
+
+  while (queue.length > 0) {
+    const currentNode = queue.shift()
+    
+    if (currentNode.val < min) min = currentNode.val
+    if (currentNode.left) queue.push(currentNode.left)
+    if (currentNode.right) queue.push(currentNode.right)
+  }
+  
+  return min;
+};
+
+
+
+
+
+/*
+Pseudocode
+Base case 1:
+    leaf node (no children) => sum is its value
+Base case 2:
+    if node is null => return negative Infinity    
+Only consider max sum between two node sums
+*/
+
+const maxPathSum = (root) => {
+  if (!root) return -Infinity // if the other child exists, it will automatically be the max
+  if (!root.left && !root.right) return root.val; // when we reach a leaf node, return its value as its sum
+  // let sum = -Infinity;
+  return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right))
+  // return sum;
+};
+
+
+
+
+
+/*
+Pseudocode
+BFS because we're checking by level
+Travel from left to right
+use a queue
+*/
+
+const bottomRightValue = (root) => {
+  // todo
+  const queue = [root]
+
+  /* fixed: */
+  let currentNode = null;
+  while (queue.length > 0) {
+    currentNode = queue.shift()
+    if (currentNode.left !== null) queue.push(currentNode.left)
+    if (currentNode.right !== null) queue.push(currentNode.right)
+  }
+
+  return currentNode.val
+
+  /* my original: */
+  // while (queue.length > 0) {
+  //   const currentNode = queue.shift()
+  //   if (currentNode.left !== null) queue.push(currentNode.left)
+  //   if (currentNode.right !== null) queue.push(currentNode.right)
+  //   if (currentNode.left === null && currentNode.right === null && queue.length === 0) return currentNode.val
+  // }
+};
+
+
+
+
+
+/*
+Gameplan:
+Iterate BFS because we want each level
+We know we're on a certain level by saving the queue size to its own variable, 
+      then creating new levels when queue size has been reduced
+*/
+
+const treeLevels = (root) => {
+  // todo
+  if (!root) return []
+  let levels = []
+  let queue = [root]
+  while (queue.length > 0) {
+    let size = queue.length;
+    let level = []
+    while (size > 0) {
+      const currentNode = queue.shift()
+      level.push(currentNode.val)
+      if (currentNode.left !== null) queue.push(currentNode.left)
+      if (currentNode.right !== null) queue.push(currentNode.right)
+      size--
+    }
+    levels.push(level)
+  }
+  return levels
+};
+
+
+
+
+
+/*
+Gameplan:
+BFS, average helper function to use on queue
+*/
+
+const levelAverages = (root) => {
+  // todo
+  if (!root) return []
+  let levels = []
+  let queue = [root]
+  while (queue.length > 0) {
+    let size = queue.length;
+    let level = []
+    while (size > 0) {
+      const currentNode = queue.shift()
+      level.push(currentNode.val)
+      if (currentNode.left !== null) queue.push(currentNode.left)
+      if (currentNode.right !== null) queue.push(currentNode.right)
+      size--
+    }
+    levels.push(getLevelAverage(level))
+  }
+  return levels
+};
+
+const getLevelAverage = level => {
+  let sum = 0;
+  for (let num of level) sum += num
+  return sum / level.length
+}
+
+
+
+
+
+/*
+Gameplan:
+DFS
+Base case 1: 
+     if root is null, return empty arr
+Base case 2:
+      if root is a leaf (no children) return arr with root's value
+Recursive call:
+      traverse through tree
+*/
+
+const leafList = (root) => {
+  // todo
+  if (root === null) return []
+  if (root.left === null && root.right === null) return [root.val]
+  
+  const leaves = []
+  leaves.push(...leafList(root.left))
+  leaves.push(...leafList(root.right))
+  return leaves
 };
